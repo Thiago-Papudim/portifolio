2f6534d713889004246cc291ad9d7d2042173b15
Thiago Soares <thiago_soares_mago@hotmail.com>
2023-11-11 20:09:04 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/thiago-papudim.png" />
-    <Titulo fontSize={20}>Thiago Soares</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Thiago Soares</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        thiago-papudim
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-end | Desenvolvedor Web | HTML | CSS | JAVASCRIPT
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
